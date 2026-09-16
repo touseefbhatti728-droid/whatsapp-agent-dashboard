541c7b2dd7b63c7dc6cd56d56921fbb91668abeb
Touseeb <touseefbhatti728@gmail.com>
2026-09-16 15:58:00 +0500

    Login/signup Resora AI design + dashboard restored

--- src/app/login/page.js
+++
@@ -1,7 +0,0 @@
-import { getBranding } from "@/lib/branding";
-import LoginForm from "@/components/LoginForm";
-
-export default async function LoginPage() {
-  const b = await getBranding();
-  return <LoginForm appName={b.app_name} />;
-}
--- src/app/signup/page.js
+++
@@ -1,7 +0,0 @@
-import { getBranding } from "@/lib/branding";
-import SignupForm from "@/components/SignupForm";
-
-export default async function SignupPage() {
-  const b = await getBranding();
-  return <SignupForm appName={b.app_name} />;
-}
